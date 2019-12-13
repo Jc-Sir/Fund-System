562bec746940f63af8f71758cab5f1c987766132
Jc-Sir <1334959005@qq.com>
2019-12-13 23:57:42 +0800

    register

---
+++ routes/api/users.js
@@ -0,0 +1,53 @@
+// for register & login
+
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+const User = require('../../models/User') //引入数据模型
+
+// $route  api/users/test
+// @desc   返回请求的json数据
+// access  public 公有接口
+router.get("/test", (req, res) => {
+    res.json({ msg: 'login works' })
+})
+
+// $router post api/users/register
+// @desc   注册
+router.post("/register", (req, res) => {
+    // 查询邮箱是否已经存在
+    console.log(req.body);
+    User.findOne({ email: req.body.email })
+        .then((user) => {
+            if (user) {
+                return res.status(400).json({ email: '邮箱已经被注册' })
+            } else {
+                const newUser = {
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar: req.body.email,
+                    password: req.body.password
+                };
+                // console.log(newUser)
+                bcrypt.genSalt(10, function(err, salt) {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        console.log(newUser.password)
+                            // Store hash in your password DB. 
+                        if (err) throw err;
+
+                        newUser.password = hash;
+
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => {
+                                console.log(err);
+                            })
+                    });
+                });
+
+            }
+        })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,20 +1,36 @@
 const express = require('express'); // 安装后引入
 const mongoose = require('mongoose'); // 安装mongoose 后引入 
+const bodyParser = require('body-parser')
 const app = express(); // 实例化
+
+// require user.js
+const users = require('./routes/api/users');
+
 
 // DB config
 const db = require("./config/config").mongodbURI // 数据库地址
 
+// use body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
 // Connect To mongodb
-mongoose.connect(db)
+mongoose.connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
     .then(() => { console.log('mongodb connected success') })
     .catch((err) => { console.log(err) })
+
+// use routes
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('hello express');
 })
+
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);

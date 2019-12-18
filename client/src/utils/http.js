8021cfe550c8718adc64d89c7069ad0b3275c4f0
Jc-Sir <1334959005@qq.com>
2019-12-18 23:50:59 +0800

    use vuex

--- client/src/utils/http.js
+++ client/src/utils/http.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { Loading, Message } from 'element-ui'
+import router from './../router/index'
 
 let loading;
 
@@ -16,18 +17,37 @@ function endLoading() {
 }
 
 // request
+axios.interceptors.request.use(
+    config => {
+        startLoading();
+        if (sessionStorage.eleToken) {
+            config.headers.Authorization = sessionStorage.eleToken;
+        }
+        return config;
+    }
+)
 
 
 
 // response 
 axios.interceptors.response.use(
     response => {
-        // endLoading
+        endLoading();
         return response;
 
     },
     error => {
         Message.error(error.response.data);
+
+        // 获取错误码
+        const { status } = error.response;
+
+        if (status == 401) {
+            Message.error('Token过期,请重新登录！');
+            sessionStorage.removeItem('eleToken');
+            router.push('/login')
+        }
+
         return Promise.reject(error);
     }
 )

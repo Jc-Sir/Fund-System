2ec81c3f7e31eefb11c0c50edb1be9353e1f68b3
Jc-Sir <44697320+Jc-Sir@users.noreply.github.com>
2019-12-27 11:30:53 +0800

    Update AppMain.vue

--- client/src/layout/components/AppMain.vue
+++ client/src/layout/components/AppMain.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <section class="app-main">
     <h1>内容区域</h1>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -13,4 +13,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.app-main {
+  /* 50= navbar  50  */
+  min-height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+}
+
+.fixed-header + .app-main {
+  padding-top: 50px;
+}
+
+.hasTagsView {
+  .app-main {
+    /* 84 = navbar + tags-view = 50 + 34 */
+    min-height: calc(100vh - 84px);
+  }
+
+  .fixed-header + .app-main {
+    padding-top: 84px;
+  }
+}
+</style>
+
+<style lang="scss">
+// fix css style bug in open el-dialog
+.el-popup-parent--hidden {
+  .fixed-header {
+    padding-right: 15px;
+  }
+}
 </style>

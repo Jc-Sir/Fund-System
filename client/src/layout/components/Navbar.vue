32081dbbe108ba66049f322711c4b4d757887725
Jc-Sir <44697320+Jc-Sir@users.noreply.github.com>
2019-12-27 11:31:00 +0800

    Update Navbar.vue

--- client/src/layout/components/Navbar.vue
+++ client/src/layout/components/Navbar.vue
@@ -1,19 +1,24 @@
 <template>
-    <div>
-        <h1>顶部导航栏</h1>
-    </div>
+  <div class="navbar">
+    <h1>顶部导航栏</h1>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                
-            }
-        },
-    }
+export default {
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
+.navbar {
+  height: 50px;
+  line-height: 50px;
+  overflow: hidden;
+  position: relative;
+  background: #fff;
+  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+}
 </style>
